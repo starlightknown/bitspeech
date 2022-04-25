e767ee310a7dea90e8cacbff11a946fe4dff7fd0
karuna tata <karunatata63@gmail.com>
2022-04-25 19:27:19 +0530

    heroku tweaks

--- bitspeech/config.py
+++ bitspeech/config.py
@@ -5,5 +5,7 @@ class Config:
     """Configuration settings for the bitspeech Flask app."""
 
     SECRET_KEY = os.environ.get("SECRET_KEY", "placeholder-secret-key")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///app.db")
+    #SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///app.db")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "postgres://xoehhrqcdxqdac:26c77c72005e69895878be54444feecd5e3f87f92a05149e08b54a92bdc12e89@ec2-34-194-158-176.compute-1.amazonaws.com:5432/da81aph83o5l17
+")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
